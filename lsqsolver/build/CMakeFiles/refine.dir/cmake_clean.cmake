404b7537790bd6ed4cc15705318248cb70ba6532
Paul Martin <pdmart@gmail.com>
2016-05-27 16:15:42 -0700

    minor updates

---
+++ lsqsolver/build/CMakeFiles/refine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/refine.dir/refine_path.cpp.o"
+  "refine.pdb"
+  "refine"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/refine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
